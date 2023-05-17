b7aaaeb0bcb4d43323708d900403d1e591b7bedc
Srinath V.S <srnthvs@gmail.com>
2023-05-16 18:10:03 -0700

    creating machine learning generative models repo
    is a repo of computer vision generative models being actively worked on.

--- CustomModelComponents/DatasetSpecific/Marvel.py
+++ CustomModelComponents/DatasetSpecific/Marvel.py
@@ -1,6 +1,32 @@
+import os
+import glob
+import csv
+
 marvel_base_image_path_train = 'D:/Downloads/Datasets/marvel_heroes/dataset/train'
 marvel_base_image_path_test = 'D:/Downloads/Datasets/marvel_heroes/dataset/test'
+dataPath = 'C:/Users/srina/PycharmProjects/MachineLearning/CustomModelComponents/DatasetSpecific/MarvelData/'
+
+def image_path_to_dataset(basePath, mode='train'):
+    hero_names = os.listdir(basePath)
+
+    completePath = []
+    for name in hero_names:
+        currentPath = basePath + '/' + name +'/'
+        files = os.listdir(currentPath)
+        files = [[currentPath + fname] for fname in files]
+        completePath.extend(files)
+
+    outputPath = dataPath + mode + '.csv'
+    with open(outputPath, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(completePath)
 
 
-def image_path_to_datset(batch_size):
-    pass
+
+
+
+
+
+if __name__ == '__main__':
+    image_path_to_dataset(marvel_base_image_path_train, mode='train')
+    image_path_to_dataset(marvel_base_image_path_test, mode = 'test')
